1e436f0a7ebe9ad970f3216bdc0f39ec87494419
Martin <martinwjwilson@gmail.com>
2020-09-09 14:36:25 +0100

    combined category_list into the category file

--- lib/category.dart
+++ lib/category.dart
@@ -1,3 +1,5 @@
+// Holds all of the categories and handles any requests to the data
+
 import 'package:flutter/material.dart';
 
 class Category {
@@ -5,5 +7,28 @@ class Category {
 
   String categoryName;
   MaterialColor categoryColour;
-  // Image categoryImage;
+// Image categoryImage;
+}
+
+class CategoryHandler {
+  List<Category> _categoryList = [
+    Category(categoryName: 'Category 1', categoryColour: Colors.orange),
+    Category(categoryName: 'Category 2', categoryColour: Colors.blue),
+    Category(categoryName: 'Category 3', categoryColour: Colors.red),
+    Category(categoryName: 'Category 4', categoryColour: Colors.green),
+    Category(categoryName: 'Category 5', categoryColour: Colors.purple),
+    Category(categoryName: 'Category 6', categoryColour: Colors.yellow)
+  ];
+
+  String getCategoryName(i) {
+    return _categoryList[i].categoryName;
+  }
+
+  MaterialColor getCategoryColour(i) {
+    return _categoryList[i].categoryColour;
+  }
+
+  int getCategoryListLength() {
+    return _categoryList.length;
+  }
 }
--- lib/screens/category_list_page.dart
+++ lib/screens/category_list_page.dart
@@ -2,7 +2,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:skincare_app/components/selection_list_card.dart';
-import 'package:skincare_app/category_data.dart';
+import 'package:skincare_app/category.dart';
 
 class CategoryListPage extends StatefulWidget {
   @override
@@ -10,8 +10,6 @@ class CategoryListPage extends StatefulWidget {
 }
 
 class _CategoryListPageState extends State<CategoryListPage> {
-  List<String> litems = ["1", "2", "Third", "4"];
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,22 +20,11 @@ class _CategoryListPageState extends State<CategoryListPage> {
         itemCount: CategoryHandler().getCategoryListLength(),
         itemBuilder: (BuildContext ctxt, int index) {
           return new SelectionListCard(
-              colour: CategoryHandler().getCategoryColour(index),
-              displayText: CategoryHandler().getCategoryName(index));
+            colour: CategoryHandler().getCategoryColour(index),
+            displayText: CategoryHandler().getCategoryName(index),
+          );
         },
       ),
     );
   }
 }
-
-//children: <Widget>[
-//for(int i = 0; i < 5; i++) {
-//SelectionListCard(colour: Colors.orange, displayText: "test");
-//};
-//SelectionListCard(colour: Colors.orange, displayText: "Category 0"),
-//SelectionListCard(colour: Colors.blue, displayText: "Category 1"),
-//SelectionListCard(colour: Colors.red, displayText: "Category 2"),
-//SelectionListCard(colour: Colors.green, displayText: "Category 3"),
-//SelectionListCard(colour: Colors.purple, displayText: "Category 4"),
-//SelectionListCard(colour: Colors.yellow, displayText: "Category 5"),
-//],
