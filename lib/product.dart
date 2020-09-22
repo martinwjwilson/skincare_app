7f4d066ae1636456dacab0edc8bd0a5ca51c6bc0
Martin <martinwjwilson@gmail.com>
2020-09-22 15:51:53 +0100

    added placeholder descriptions for test products

--- lib/product.dart
+++ lib/product.dart
@@ -6,10 +6,12 @@ class Product {
   Product(
       {@required this.productName,
       @required this.categoryName,
+      this.productDescription,
       @required this.productColour});
 
   String productName;
   String categoryName;
+  String productDescription;
   Color productColour;
   // Image productSplashImage;
 }
@@ -35,27 +37,32 @@ class ProductHandler {
     Product(
         productName: 'Product 1',
         categoryName: 'Category 1',
+        productDescription: 'Description of product 1 goes in here',
         productColour: Color(0xFFFFFFCC)),
     Product(
         productName: 'Product 2',
         categoryName: 'Category 1',
+        productDescription: 'Description of product 2 goes in here',
         productColour: Color(0xFFFFEECC)),
     Product(
-      productName: 'Product 3',
-      categoryName: 'Category 2',
-      productColour: Color(0xFFFFDDCC),
-    ),
+        productName: 'Product 3',
+        categoryName: 'Category 2',
+        productDescription: 'Description of product 3 goes in here',
+        productColour: Color(0xFFFFDDCC)),
     Product(
         productName: 'Product 4',
         categoryName: 'Category 2',
+        productDescription: 'Description of product 4 goes in here',
         productColour: Color(0xFFFFCCCC)),
     Product(
         productName: 'Product 5',
         categoryName: 'Category 3',
+        productDescription: 'Description of product 5 goes in here',
         productColour: Color(0xFFFFBBCC)),
     Product(
         productName: 'Product 6',
         categoryName: 'Category 4',
+        productDescription: 'Description of product 6 goes in here',
         productColour: Color(0xFFFFAACC))
   ];
 }
